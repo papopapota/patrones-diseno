c6113f62ee6e69cad08fd524e2011e07cb463c3e
papopapota <daniel_olivares09@hotmail.com>
2026-06-26 10:48:48 -0500

    patron decorator

--- 02-estructurales/04-decorator.ts
+++ 02-estructurales/04-decorator.ts
@@ -11,3 +11,64 @@
  *
  * https://refactoring.guru/es/design-patterns/decorator
  */
+import { COLORS } from '../helpers/colors.ts';
+
+
+interface Notification {
+    send(message: string): void;
+
+}
+
+class BasicNotification implements Notification {
+    send(message: string): void {
+        console.log(`%cEnviando notificación básica: %c${message}`, COLORS.blue, COLORS.white);
+    }
+}
+
+// clase decoradora abstracta
+
+abstract class NotificationDecorator implements Notification {
+
+    protected notification: Notification;
+    constructor(
+        notification: Notification
+    ) {
+        this.notification = notification;
+    }
+    send(message: string): void {
+        this.notification.send(message);
+    }
+}
+
+//crear diferentes decoradores
+
+class EmailDecorator extends NotificationDecorator {
+    
+    private sendEmail(message: string): void {
+        console.log(`%cEnviando notificación por correo: %c${message}`, COLORS.purple, COLORS.white);
+    }
+    override send(message: string): void {
+        super.send(message);
+        this.sendEmail(message);
+    }
+}
+
+class SMSDecorator extends NotificationDecorator {
+    
+    private sendSMS(message: string): void {
+        console.log(`%cEnviando notificación por SMS: %c${message}`, COLORS.orange, COLORS.white);
+    }
+    override send(message: string): void {
+        super.send(message);
+        this.sendSMS(message);
+    }
+}
+function main() {
+    let notification: Notification = new BasicNotification();
+    notification = new EmailDecorator(notification);
+    notification = new SMSDecorator(notification);
+
+    notification.send('Alerta del sistema');
+}
+
+main();
--- 02-estructurales/04.2-decorator.ts
+++ 02-estructurales/04.2-decorator.ts
@@ -20,12 +20,13 @@ interface Character {
 
 // 2. Clase BasicCharacter
 // Representa un personaje básico sin accesorios
-// TODO: Implementar la interfaz Character
-class BasicCharacter {
-  // TODO: Implementar los métodos de la interfaz
-  // getDescription: return 'Personaje básico';
-  // TODO: Implementar los métodos de la interfaz
-  // getStats: return { attack: 10, defense: 10 };
+class BasicCharacter implements Character {
+  getDescription(): string {
+    return 'Personaje básico';
+  }
+  getStats(): { attack: number; defense: number; } {
+    return { attack: 10, defense: 10 };
+  }
 }
 
 // 3. Clase Decoradora CharacterDecorator
@@ -33,19 +34,17 @@ class BasicCharacter {
 abstract class CharacterDecorator implements Character {
   // Protected para que las clases hijas puedan acceder a la propiedad
   // Private no permitiría que las clases hijas accedan a la propiedad
-  // TODO: Definir la propiedad protected character de tipo Character
-
-  // TODO: Definir el constructor que recibe un personaje de tipo Character
-
-  // TODO: Implementar los métodos de la interfaz Character, pero retornando
+  protected character: Character;
+  constructor(character: Character) {
+    this.character = character;
+  }
   // la descripción y estadísticas del personaje decorado
   getDescription(): string {
-    throw new Error('Method not implemented.');
+    return this.character.getDescription();
   }
 
-  // TODO: Implementar los métodos de la interfaz Character
   getStats(): { attack: number; defense: number } {
-    throw new Error('Method not implemented.');
+    return this.character.getStats();
   }
 }
 
@@ -90,6 +89,17 @@ class SwordDecorator extends CharacterDecorator {
 
 // TODO: Crear un nuevo decorador que añada un anillo que aumenta el ataque en +3
 // class RingDecorator ...
+
+class RingDecorator extends CharacterDecorator {
+  override getDescription(): string {
+    return this.character.getDescription() + '\n * con Anillo';
+  }
+
+  override getStats(): { attack: number; defense: number; } {
+    const stats = this.character.getStats();
+    return { attack: stats.attack + 3, defense: stats.defense };
+  }
+}
 
 // 7. Código Cliente para Probar el Decorador
 
