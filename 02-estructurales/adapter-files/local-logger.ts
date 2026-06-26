23ab7c86dbdb1eceb249e1b61956fdb8550f7e08
papopapota <daniel_olivares09@hotmail.com>
2026-06-26 10:49:32 -0500

    patron adapter

--- 02-estructurales/adapter-files/local-logger.ts
+++ 02-estructurales/adapter-files/local-logger.ts
@@ -1,3 +1,23 @@
 import { COLORS } from '../../helpers/colors.ts';
 
 // TODO: Implementar el LocalLogger Class
+
+export class LocalLogger {
+    constructor(
+        private file: string
+    ) {
+
+    }
+
+    writeLog(message: string): void {
+        console.log(`[ ${this.file} Log] %c${message}`, COLORS.green);
+    }
+
+    writeError(message: string): void {
+        console.log(`[ ${this.file} Error] %c${message}`, COLORS.red);
+    }
+
+    writeWarning(message: string): void {
+        console.log(`[ ${this.file} Warning] %c${message}`, COLORS.yellow);
+    }
+}
--- 02-estructurales/adapter-files/logger-adapter.ts
+++ 02-estructurales/adapter-files/logger-adapter.ts
@@ -1,3 +1,40 @@
 import { Logger } from 'jsr:@deno-library/logger';
+import { COLORS } from '../../helpers/colors.ts';
 
 // TODO: Implementar el LoggerAdapter
+interface ILoggerAdapter {
+    file: string;
+    writeLog(message: string): void
+    writeError(message: string): void
+    writeWarning(message: string): void
+}
+
+
+export class DenoLoggerAdapter implements ILoggerAdapter {
+    public file: string;
+    private logger = new Logger();
+
+    constructor(file: string) {
+        this.file = file;
+    }
+
+    writeLog(message: string): void {
+        this.logger.info(`[ ${this.file} Log] %c${message}`, COLORS.green);
+    }
+    writeError(message: string): void {
+        this.logger.error(`[ ${this.file} Log] %c${message}`, COLORS.red);
+    }
+    writeWarning(message: string): void {
+        this.logger.warn(`[ ${this.file} Log] %c${message}`, COLORS.yellow);
+    }
+
+}
+
+
+// const logger = new Logger();
+
+// logger.info('Este es un mensaje de log');
+// logger.error('Este es un mensaje de error');
+// logger.warning('Este es un mensaje de advertencia');
+
+
